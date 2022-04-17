01558d26455d9996e29740d16095bcff94c3a750
alamindjp <alaminhossaindjp@gmail.com>
2022-04-18 05:36:59 +0600

    added the three question and  answer

--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -2,8 +2,33 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>This is Blogs Page</h1>
+        <div className='container w-75 mt-5 text-start'>
+            <div className='border border-danger rounded p-3 mb-3'>
+                <h4>Q 1. Difference between authorization and authentication.</h4>
+                <p className='text-start'><span className='fs-5 fw-bolder'>Ans: </span> <br /> <span className='fw-bolder fst-italic'>Authorization</span> is the permissions. Authorization determines what resources a user can access.<br />
+                    <span className='fw-bolder fst-italic'> Authentication</span> in the form of a key. The lock on the door only grants access to someone with the correct key in much the same way that a system only grants access to users who have the correct credentials.</p>
+            </div>
+
+            <div className='border border-danger rounded p-3 mb-3'>
+                <h4>Q 2. Why are you using firebase? What other options do you have to implement authentication?</h4>
+                <p><span className='fs-5 fw-bolder'>Ans: </span> <br /> <span className='fw-bolder fst-italic'>Firebase</span> is used for many purposes that can help your app to develop it a quality app and its security is high lavel. <br />
+
+                    <span className='fw-bolder fst-italic'>1.</span> Password-based authentication. <br />
+                    <span className='fw-bolder fst-italic'>2.</span> Two-factor/multifactor authentication. <br />
+                    <span className='fw-bolder fst-italic'>3.</span> Biometric authentication. <br />
+                    <span className='fw-bolder fst-italic'>4.</span> Token-based authentication.
+                </p>
+            </div>
+
+            <div className='border border-danger rounded p-3 mb-3'>
+                <h4>Q 1. What other services does firebase provide other than authentication.</h4>
+                <p className='text-start'><span className='fs-5 fw-bolder'>Ans: </span> <br />
+                    <span className='fw-bolder fst-italic'>1. Parse-</span> Open Source Backend Platform. <br />
+                    <span className='fw-bolder fst-italic'>2. Kinvey-</span>  Mobile Backend as a Service (mBaaS) for the Enterprise <br />
+                    <span className='fw-bolder fst-italic'>3. Back4app-</span> Parse Hosting Platform. <br />
+                    <span className='fw-bolder fst-italic'>4. Kuzzle-</span> Backend for web, hybrid, or native mobile apps and IoT projects.
+                </p>
+            </div>
         </div>
     );
 };
--- src/pages/sharePage/Header/Header.js
+++ src/pages/sharePage/Header/Header.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./Header.css"
+import img from "./image/4044(1).png";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="container-fluid bg-primary py-3">
-      <nav>
-        <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to="/">Home</NavLink>
-        <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to="/blogs">Blogs</NavLink>
-        <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to="/about">About</NavLink>
+    <header className="container-fluid bg-primary py-3 mb-2">
+      <nav className="d-flex">
+        <div>
+          <img className="logo-img" src={img} alt="" />
+        </div>
+        <div className="">
+          <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to="/">Home</NavLink>
+          <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to="/blogs">Blogs</NavLink>
+          <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to="/about">About</NavLink>
+        </div>
       </nav>
     </header>
   );
